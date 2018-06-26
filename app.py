ed3d07a70f0af8883765c887f5f5c0055e501fbe
hamlees93 <33846980+hamlees93@users.noreply.github.com>
2018-06-26 13:18:23 +1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template('idex.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=6000,debug=True)
