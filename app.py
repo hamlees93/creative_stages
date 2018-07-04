9536e4f2bf96a4e555b1f2d1a67a17bab2d6c6f7
hamlees93 <33846980+hamlees93@users.noreply.github.com>
2018-07-04 11:23:40 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for, jsonify
+from flask_bootstrap import Bootstrap 
+from forms import ContactForm
+from flask_mail import Message, Mail 
 
+mail = Mail()
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return render_template('index.html')
+#Add when website is ready to launch
+#app.config['RECAPTCHA_PUBLIC_KEY'] = ''
+#app.config['RECAPTCHA_PRIVATE_KEY'] = ''
 
-if __name__ == '__main__':
-    
+#Find out the details of info@creatingstages.com.au
+app.config['SECRET_KEY'] = '********************'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'bluemountainsblend@gmail.com'
+app.config['MAIL_PASSWORD'] = '********'
+mail.init_app(app)
+
+Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  form = ContactForm()
+  if request.method == 'POST':
+    if form.validate() == False:
+      flash('All fields required.')
+      return render_template('index.html', form=form)
+    else:
+      msg = Message(form.name.data, sender='bluemountainsblend@gmail.com', recipients=['info@creatingstages.com.au'])
+      msg.body = """
+      From : %s <%s>
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+      mail.send(msg)
+      return render_template('success.html', form=form)
+  elif request.method == 'GET':
+    return render_template('index.html', form=form)
+
+@app.route('/book')
+def book():
+  return render_template('book.html')
+
+@app.route('/success')
+def success():
+  return render_template('success.html')
